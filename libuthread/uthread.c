841e433d6d0ae6130edcc3c2d8faf130f7e7d19c
alevchen <alevchen@ad3.ucdavis.edu@pc28.cs.ucdavis.edu>
2019-04-30 16:20:34 -0700

    start creating thread library

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -11,7 +11,35 @@
 #include "queue.h"
 #include "uthread.h"
 
-/* TODO Phase 2 */
+typedef struct tcb* tcb_t;
+
+// this enum lets us keep track of the current state of the thread
+enum threadState { READY, RUNNING, BLOCKED, ZOMBIE };
+
+//create a struct that will hold each thread's execution content
+
+struct tcb {
+  uthread_t tid; // this is the thread id
+  enum threadState; // the current state of the thread
+  uthread_ctx_t context; //the context of the thread
+  void *stack; // the stack belonging to the thread
+};
+
+// this function initializes the uthread library by registering
+// the single execution flow of the application as the main thread.
+
+int uthread_init() {
+  // create enough memory for the thread
+  tcb_t main = malloc(sizeof(struct tcb));
+
+  // initialize the main thread
+  main->tid = 0;
+  main->threadState = READY;
+  main->content = NULL;
+
+
+
+}
 
 void uthread_yield(void)
 {
@@ -38,4 +66,3 @@ int uthread_join(uthread_t tid, int *retval)
 	/* TODO Phase 2 */
 	/* TODO Phase 3 */
 }
-
---
+++ test/test_queue.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "queue.h"
+
+//create the queue that we will be testing
+
+void test_queue(){
+
+}
+
+int main(){
+  return 0;
+}
