faf5bed1c930974ef97d7b2ce24e8d942d330f76
alevchenko714 <alevchenko@ucdavis.edu>
2019-05-01 21:02:11 -0700

    timer and alarm

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,6 +15,17 @@
  */
 #define HZ 100
 
+
+// need timer to fire off 100 times per second, so 1 sec/100
+// which is 100000/HZ
+
+// use the signal to force the current thread to yield
+void handlerFunction(int signum) {
+  // uthread_yield();
+  static int count = 0;
+ printf ("timer expired %d times\n", ++count);
+}
+
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
@@ -25,8 +36,30 @@ void preempt_enable(void)
 	/* TODO Phase 4 */
 }
 
+
+// reference of how to use sigaction and timer together
+// http://www.informit.com/articles/article.aspx?p=23618&seqNum=14
 void preempt_start(void)
 {
-	/* TODO Phase 4 */
+  struct sigaction sa;
+  struct itimerval timer;
+
+  memset (&sa, 0, sizeof (sa));
+  sa.sa_handler = &handlerFunction;
+
+  sigaction(SIGVTALRM, &sa, NULL);
+
+  // set timer to go off 100 times per second
+  timer.it_value.tv_sec = 0;
+  timer.it_value.tv_usec = 100000/HZ;
+  timer.it_interval.tv_sec = 0;
+  timer.it_interval.tv_usec = 100000/HZ;
+
+  settimer(ITIMER_VIRTUAL, &timer, NULL);
+
+
 }
 
+int main() {
+  preempt_start();
+}
